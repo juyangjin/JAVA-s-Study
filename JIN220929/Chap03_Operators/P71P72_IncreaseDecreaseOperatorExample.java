07cd30ed4fe79f0b68be848213649fa0508386fb
juyangjin <wndid2008@gmail.com>
2022-09-29 23:45:35 +0900

    2022/09/29

---
+++ JIN220929/Chap03_Operators/P71P72_IncreaseDecreaseOperatorExample.java
@@ -0,0 +1,26 @@
+package JIN220929.Chap03_Operators;
+
+public class P71P72_IncreaseDecreaseOperatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		int y = 10;
+		int z;
+		
+		System.out.println("-------------------");
+		x++;
+		++x;
+		System.out.println("x=" + x);
+		
+		System.out.println("-------------------");
+		y--;
+		--y;
+		System.out.println("y=" );
+		System.out.println("-------------------");
+		System.out.println("-------------------");
+		System.out.println("-------------------");
+
+	}
+
+}
