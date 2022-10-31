b4bf36273cbc21d2d0bf135e87e7eabf9a5835da
juyangjin <wndid2008@gmail.com>
2022-10-31 17:10:35 +0900

    221031

---
+++ j221031/cahp08_Interface/P347_RemoteControl.java
@@ -0,0 +1,27 @@
+package j221031.cahp08_Interface;
+
+public class P347_RemoteControl {
+	//상수
+	public int MAX_VOLUME = 10;
+	public int MIN_VOLUME = 0;
+	
+	//추상 메소드
+	void turnOn();
+	void turnOff();
+	void setVolume(int volume);
+	
+	//디폴트 메소드
+	default void setMute(boolean mute) { //실행 내용까지 작성
+		if(mute) {
+			System.out.println("무음 처리합니다.");
+		} else {
+			System.out.println("무음 해제합니다.");
+		}
+	}
+	
+	//정적 메소드
+	static void changeBattery() {
+		System.out.println("건전지를 교환합니다.");
+	}
+
+}
---
+++ j221031/cahp08_Interface/P356_Searchable.java
@@ -0,0 +1,6 @@
+package j221031.cahp08_Interface;
+
+public class P356_Searchable {
+	void search(String url);
+
+}
