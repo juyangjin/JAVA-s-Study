184dd90ffe74d2ce83ad43ab725fbbfdf877a980
juyangjin <wndid2008@gmail.com>
2022-11-03 17:50:29 +0900

    수정

---
+++ j221027/chap07_Inheritance/Calculator.java
@@ -0,0 +1,9 @@
+package j221027.chap07_Inheritance;
+
+public class Calculator {
+	double areaCircle(double r) {
+		System.out.println("Calulator 객체의 areaCircle() 실행");
+		return 3.14159 * r * r;
+	}
+
+}
---
+++ j221027/chap07_Inheritance/P300_SupersonicAirplaneExmple.java
@@ -0,0 +1,10 @@
+package j221027.chap07_Inheritance;
+
+public class P300_SupersonicAirplaneExmple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
